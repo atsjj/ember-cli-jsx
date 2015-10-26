48bd9c88bdc203f8a3cfd51d925d3f4019dba978
Steve Jabour <steve@jabour.me>
2015-10-26 16:45:13 -0600

    Debugging

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ module.exports = {
       name: 'ember-cli-jsx',
       ext: 'jsx',
       toTree: function(tree) {
-        return require('broccoli-babel-transpiler')(tree, getBabelOptions(addon));
+        return require('broccoli-babel-transpiler')(tree, {
+          filterExtensions: ['jsx'],
+          whitelist:        ['react']
+        });
       }
     });
   },
@@ -32,17 +35,3 @@ module.exports = {
     }
   }
 };
-
-function getAddonOptions(addonContext) {
-  var baseOptions = (addonContext.parent && addonContext.parent.options) || (addonContext.app && addonContext.app.options);
-  return baseOptions && baseOptions.jsx || {};
-}
-
-function getBabelOptions(addonContext) {
-  var options = clone(getAddonOptions(addonContext));
-
-  // Ensure modules aren't compiled unless explicitly set to compile
-  options.whitelist = options.whitelist || ['react'];
-
-  return options;
-}
